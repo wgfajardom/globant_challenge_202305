3bd9d0acd90591ce9e015536c8d4f1acd9cab822
Wilmar Fajardo <wilmargfm1021@gmail.com>
2023-05-12 15:52:16 -0600

    MOD: integration of API and database
    The API and PostgreSQL database are integrated. Only the GET method for retrieving departments is implemented,

--- app_globant.py
+++ app_globant.py
@@ -4,37 +4,86 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 import pandas as pd
+import psycopg2
+import sys
+
 
 ### Initialize API
 app_globant = FastAPI()
 
-### HTTP methods for departments
 
-# Function for next department_id
-def _find_next_id_dep():
-    return max(getattr(elem,'department_id') for elem in departments) + 1
+### Database connection
+CONNECT_DB = "host=localhost port=5432 dbname=globant user=postgres password=GlobantChallenge"
+try:
+    # Make connection to db
+    cxn = psycopg2.connect(CONNECT_DB)    
+    # Create a cursor to db
+    cur = cxn.cursor()
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+    sys.exit()
+
+
+### Definition of Classes
 
 # Class Department
 class Department(BaseModel):
-    department_id: int = Field(default_factory=_find_next_id_dep, alias="id")
+    department_id: int = Field(alias="id")
     name: str
 
-# Read departments file and it into a Pandas DataFrame
-dep_filename = 'departments.csv'
-df_dep = pd.read_csv(dep_filename, names=['id','name'], sep=',')
 
-# Load departments using the class Deparment
-departments=[]
-for index, row in df_dep.iterrows():
-    aux_dep = Department(id=row['id'], name=row['name'])
-    departments.append(aux_dep)
+
+### HTTP methods for departments
+
+# Retriving data from database
+
+def select_table(cur, table):
+    cur.execute("SELECT * FROM {};".format(table))
+    raw_ls = list(cur.fetchall())
+    if len(raw_ls) > 0:
+        output_ls = [Department(id=elem[0], name=elem[1]) for elem in raw_ls]
+    else:
+        output_ls = []
+    return output_ls
+
+# # Read file and copy to database
+# dep_filename = 'departments.csv'
+# with open(dep_filename, 'r') as f:
+#     cur.copy_from(f, 'companies', sep=",")
+#     cxn.commit()
+
+# # Retriving data from database
+# cur.execute("SELECT * FROM departments;")
+# departments = list(cur.fetchall())
+
+# # Load departments using the class Deparment
+# departments=[]
+# for index, row in df_dep.iterrows():
+#     aux_dep = Department(id=row['id'], name=row['name'])
+#     departments.append(aux_dep)
 
 # GET request for retrieving the list of departments
 @app_globant.get("/departments")
 async def get_departments():
-    return departments
+    return select_table(cur, 'companies')
 
-# @app.post("/countries", status_code=201)
-# async def add_country(country: Country):
-#     countries.append(country)
+# @app.post("/departments", status_code=201)
+# async def add_deparments():
+
+#     # Read file and copy to database
+#     dep_filename = 'departments.csv'
+#     with open(dep_filename, 'r') as f:
+#         cur.copy_from(f, 'departments', sep=",")
+#         cxn.commit()
+#         cur.execute("SELECT * FROM departments;")
+#         departments = list(cur.fetchall())
+
 #     return country
+
+
+
+# #closing database connection.
+#     if(cxn):
+#         cur.close()
+#         cxn.close()
+#     print("PostgreSQL connection is closed")
