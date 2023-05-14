40e990d76544f7c4303e0730dedc97aeeeb728be
Wilmar Fajardo <wilmargfm1021@gmail.com>
2023-05-13 23:21:36 -0600

    MOD: fully functional POST method
    Some nice changes delivered which are listed below.
    
    1. Table Class is deleted due to it was unnecessary. As consequence, the body of the POST method is simplified.
    2. Condition that evaluates if parameter sta_ind is negative. In such case, the API returns a message indicating that negative sta_ind is not allowed.
    3. Verification of ID field before inserting into the table. If the register to insert has an existing ID, then the register is not inserted.
    4. Change in the return for POST method. Now returns a text with all the information about the batch load operation.
    5. GET method uses function retrieve_table, while POST method uses function batch_load.
    
    The GET and POST methods only work for departments table so far.

--- app_globant.py
+++ app_globant.py
@@ -37,10 +37,6 @@ class File(BaseModel):
     sta_ind: Optional[int] = 1
     end_ind: Optional[int] = 1000
 
-# Class File
-class Table(BaseModel):
-    tablename: str
-
 # Class Department
 class Department(BaseModel):
     department_id: int = Field(alias="id")
@@ -49,11 +45,11 @@ class Department(BaseModel):
 
 
 
-### HTTP methods for departments
+### Core functions for HTTP methods
 
-# Retriving data from database
-def select_table(cur, table):
-    cur.execute("SELECT * FROM {};".format(table))
+# Retriving data from database (for GET methods)
+def retrieve_table(cur, table):
+    cur.execute("SELECT * FROM {} ORDER BY id;".format(table))
     raw_ls = list(cur.fetchall())
     if len(raw_ls) > 0:
         output_ls = [Department(id=elem[0], name=elem[1]) for elem in raw_ls]
@@ -62,41 +58,92 @@ def select_table(cur, table):
     return output_ls
 
 
-# GET request for retrieving the list of departments
-@app_globant.get("/departments")
-async def get_departments():
-    return select_table(cur, 'departments')
+# Batch load to database (for POST methods)
+def batch_load(cur, filename, sta_ind, end_ind, cols_table):
 
-
-# POST request to load data into the database
-@app_globant.post("/departments", status_code=201)
-async def add_deparments(file: File, table: Table):
-
-    # Load parameters
-    filename = file.filename
-    sta_ind = file.sta_ind
-    end_ind = file.end_ind
     nrows = end_ind-sta_ind+1
-    tablename = table.tablename
-    cols_dep = ["id", "name"]
+    tablename = filename.split(".")[0]
 
-    if (end_ind >= sta_ind) and (nrows <= 1000):  
+    if (sta_ind >= 1) and (end_ind >= sta_ind) and (nrows <= 1000):  
+
         # Read file using range of indexes
-        df = pd.read_csv(filename, sep=",", names=cols_dep, skiprows=sta_ind-1, nrows=nrows)
+        df = pd.read_csv(filename, sep=",", names=cols_table, skiprows=sta_ind-1, nrows=nrows)
+
+        # List of ids to insert
+        input_ids = set(list(df["id"].values))
+
+        # List of existing ids in the destination table
+        cur.execute("SELECT id FROM {};".format(tablename))
+        exist_ids = set([tup[0] for tup in cur.fetchall()])
+
+        # Filter ids that already exist on the table
+        if exist_ids != {}:
+            allow_ids = list(input_ids.difference(exist_ids))
+            nonal_ids = list(input_ids.intersection(exist_ids))
+            df = df[df["id"].isin(allow_ids)]
+        else:
+            allow_ids = list(input_ids)
+            nonal_ids = []
+
         # Process file for inserting in the table
         tuples = [tuple(x) for x in df.to_numpy()]
         cols = ','.join(list(df.columns))
+
         # Insert data into table
         query = "INSERT INTO %s(%s) VALUES %%s" % (tablename, cols)
         extras.execute_values(cur, query, tuples)
         cxn.commit()
-        return select_table(cur, 'departments')
+
+        # Response
+        url_get_departments = "http://127.0.0.1:8000/{}".format(tablename)
+        la_ids = len(allow_ids)
+        ln_ids = len(nonal_ids)
+        if (la_ids > 0) and (ln_ids > 0):
+            response = {"Batch load successful. In total {} registers were inserted. They correspond to ids {}. There were {} registers that already exist on table {}, corresponding to ids {}. Check your results using {}".format(la_ids, allow_ids, ln_ids, tablename, nonal_ids, url_get_departments)}
+        elif (ln_ids == 0):
+            response = {"Batch load successful. In total {} registers were inserted. They correspond to ids {}. There were not existing registers on table {}. Check your results using {}.".format(la_ids, allow_ids, tablename, url_get_departments)}
+        elif (la_ids == 0):
+            response = {"Batch load unsuccessful. No registers were inserted, because all of them already exist on table {}. Check the table using {}".format(tablename, url_get_departments)}
+        else:
+            response = {"Somehow you broke this API. At least some text is shown."}
+
+        return response
 
     else:
         if nrows > 1000:
             return {"Consider that 1000 is the maximum amount of registers to load in a single request. You are trying to insert {} registers.".format(nrows)}
+        elif sta_ind <= 0:
+            return {"The parameter sta_ind must be greater or equal than 1."}
         else:
             return {"There is an issue with the indexes. Take into account that end_ind should be greater or equal than sta_ind."}
+
+
+
+### HTTP methods
+
+# GET request for retrieving the list of departments
+@app_globant.get("/departments")
+async def get_departments():
+    # Call retrieve_table function
+    return retrieve_table(cur, 'departments')
+
+
+# POST request to load data into the database
+@app_globant.post("/departments", status_code=201)
+async def add_deparments(file: File):
+    
+    # Load parameters
+    filename = file.filename
+    sta_ind = file.sta_ind
+    end_ind = file.end_ind
+
+    # Parameter exclusive for the table departments
+    cols_table = ["id", "name"]
+
+    # Call batch_load function
+    return batch_load(cur, filename, sta_ind, end_ind, cols_table)
+
+
 
 # # Closing database connection.
 # if(cxn):
